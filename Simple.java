6625c5a3865123a9cb35fc2b431cd01fb37aaf91
Shubhambh18 <mindfak3@outlook.com>
2019-08-30 02:56:46 -0700

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,5 +2,6 @@ class Simple{
 public static void main(String args[]) throws InterruptedException{
 Thread.sleep(1000);
 System.out.println("Hello JAVA");
+ System.out.println("Hello JAVA Updated");
 }
 }
